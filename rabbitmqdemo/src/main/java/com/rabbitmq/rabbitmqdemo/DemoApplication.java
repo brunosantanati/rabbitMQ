d4f8d5fa78b3968bede32d497e49ed1f8e9f65d6
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-08-13 16:16:37 -0300

    Envio da primeira mensagem para fila do RabbitMQ.

--- rabbitmqdemo/src/main/java/com/rabbitmq/rabbitmqdemo/DemoApplication.java
+++ rabbitmqdemo/src/main/java/com/rabbitmq/rabbitmqdemo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.rabbitmq.rabbitmqdemo;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//rabbitTemplate.convertAndSend("Hello from our first message!"); //Envia para o default exchange
+		rabbitTemplate.convertAndSend("TestExchange", "testRouting", "Hello from code!");
+	}
 }

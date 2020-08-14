947649e02140b8d2d6730c032b1bd2bc903eea52
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-08-14 15:54:03 -0300

    Desserializar objeto no aplicacao que consome as mensagens da fila.

--- rabbitlistenerproject/src/main/java/com/example/rabbitlistenerproject/RabbitMQMessageListener.java
+++ rabbitlistenerproject/src/main/java/com/example/rabbitlistenerproject/RabbitMQMessageListener.java
@@ -1,14 +1,25 @@
 package com.example.rabbitlistenerproject;
 
-import org.springframework.amqp.core.AcknowledgeMode;
+import com.rabbitmq.rabbitmqdemo.SimpleMessage;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 
 public class RabbitMQMessageListener implements MessageListener {
     @Override
     public void onMessage(Message message) {
-        System.out.println("message = [" + new String(message.getBody()) + "]");
+
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(message.getBody());
+            ObjectInput in = new ObjectInputStream(bis);
+            SimpleMessage simpleMessage = (SimpleMessage) in.readObject();
+
+            System.out.println("message = [" + simpleMessage + "]");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
